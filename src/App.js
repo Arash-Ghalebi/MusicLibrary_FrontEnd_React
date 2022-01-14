07770694ac4c31b18d6210d94b538a056329f8c1
Arash Ghalebi <aghalebi33@gmail.com>
2022-01-14 14:41:12 -0500

    Test to establish connection between App component and child components for the Music Library.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+//import React, { useState } from 'react';
+import MusicTable from './Components/MusicTable/MusicTable';
 
 function App() {
+
+  //const [entries, setEntries] = useState()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div>
+        <h3>Hello World</h3>
+      </div>
+
+      <div>
+        <MusicTable />
+      </div>
+    </div> 
+
   );
 }
 
---
+++ src/Components/MusicTable/MusicTable.jsx
@@ -0,0 +1,9 @@
+const MusicTable = (props) => {  
+    return (
+      <div>
+        <h5>Music Table test</h5>
+      </div>
+    );
+  }
+
+  export default MusicTable;
